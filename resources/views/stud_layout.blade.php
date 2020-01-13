4c3db24f375bffadc1860c55498367549968b459
maulik46 <maulikp4646@gmail.com>
2020-01-13 17:58:24 +0530

    3rd change

--- resources/views/stud_layout.blade.php
+++ resources/views/stud_layout.blade.php
@@ -102,7 +102,7 @@
                                 <li class="my-3">
                                     <a href="{{url('/winner-list')}}">
                                         <i data-feather="award"></i>
-                                        <span> Winners List </span>
+                                        <span> Winner List </span>
                                     </a>
                                 </li>
                                 <li class="my-3">
