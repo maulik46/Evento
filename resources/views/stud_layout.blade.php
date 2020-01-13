f5815d5522ee6213bd75ee7d386ed5f21dd43f07
maulik46 <maulikp4646@gmail.com>
2020-01-13 18:03:21 +0530

    change winner list

--- resources/views/stud_layout.blade.php
+++ resources/views/stud_layout.blade.php
@@ -102,7 +102,7 @@
                                 <li class="my-3">
                                     <a href="{{url('/winner-list')}}">
                                         <i data-feather="award"></i>
-                                        <span>My Winner List </span>
+                                        <span>Winner List </span>
                                     </a>
                                 </li>
                                 <li class="my-3">
