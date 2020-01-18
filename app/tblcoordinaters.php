3edc9c5a41353f95c8d690d51f4f68c6480fe038
yash-201 <59959814+yash-201@users.noreply.github.com>
2020-01-18 17:23:12 +0530

    coordinate model

---
+++ app/tblcoordinaters.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class tblcoordinaters extends Model
+{
+    //
+    public $timestamps=false;
+    protected $table="tblcoordinaters";
+}
