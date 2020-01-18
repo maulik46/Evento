0d68a0ddba0d8c99b76dc0af9ec0c30a1064e0d0
parth177 <59960568+parth177@users.noreply.github.com>
2020-01-18 21:32:37 +0530

    notice dynamic

--- resources/views/notice.blade.php
+++ resources/views/notice.blade.php
@@ -9,42 +9,61 @@
                         </h5>
                    </div> -->
                    <div>
+                   <?php $c=0;?>
+                   @foreach($notice as $nt)
+                   <?php 
+                        $c++;
+                        if($c==1)
+                        {
+                            $new=App\tblstudent::select('last_noti')->where('senrl',Session::get('senrl'))->first();
+                            \DB::table('tblstudent')->where('senrl',Session::get('senrl'))->update(['last_noti'=>$nt->nid]);
+                        }
+                    ?>
+                    @if($nt->nid > $new['last_noti'])
                        <div class="card new-shadow-sm my-2 rounded-0 hover-me-sm" style="border-left: 4px solid #ff5c75;">
                            <div class="card-body py-2">
                                <div class="d-flex justify-content-between align-items-center flex-wrap">
-                                    <span class="badge badge-soft-primary px-3 py-1 badge-pill">28/12/2019</span>
-                                    <h6>Dr.Rohit Radadiya</h6>
+                                    <span class="badge badge-soft-primary px-3 py-1 badge-pill">{{date('d/m/Y',strtotime($nt->ndate))}}</span>
+                                    <h6>{{ucfirst($nt->sender)}}</h6>
                                </div>
                                <div>
-                                    <h5 class="mt-0">Picnic Notice</h5>
+                                    <h5 class="mt-0">{{ucfirst($nt->topic)}}</h5>
                                     <div class="card-text mb-1"> 
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non tenetur possimus
-                                        adipisci?
+                                        {{ucfirst($nt->message)}}
+                                    </div>
+                                    @if($nt->attechment)
+                                    <div class="card-action my-2">
+                                        <a href="{{asset('attechment')}}/{{$nt->attechment}}" class="btn btn-soft-danger rounded-sm new-shadow-sm font-weight-bold px-3 mr-1">{{$nt->attechment}}</a>
+                                        
                                     </div> 
+                                    @endif
                                </div>
                            </div>
                        </div>
-                       <div class="card new-shadow-sm my-2 rounded-0 hover-me-sm" style="border-left: 4px solid #1ae1ac;">
+                       @else
+                       <div class="card new-shadow-sm my-2 rounded-0 hover-me-sm" style="border-left: 4px solid #1AE1AC;">
                            <div class="card-body py-2">
                                <div class="d-flex justify-content-between align-items-center flex-wrap">
-                                   <span class="badge badge-soft-primary px-3 py-1 badge-pill">28/12/2019</span>
-                                   <h6>Dr.Rohit Radadiya</h6>
+                                    <span class="badge badge-soft-primary px-3 py-1 badge-pill">{{date('d/m/Y',strtotime($nt->ndate))}}</span>
+                                    <h6>{{ucfirst($nt->sender)}}</h6>
                                </div>
                                <div>
-                                    <h5 class="mt-0">Picnic Notice</h5>
-                                    <div class="card-text">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non tenetur possimus
-                                        adipisci?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis explicabo itaque fugit pariatur adipisci. Quod, pariatur corrupti, laborum eius quaerat eos vero, necessitatibus inventore nihil provident ut consequuntur soluta magnam.
+                                    <h5 class="mt-0">{{ucfirst($nt->topic)}}</h5>
+                                    <div class="card-text mb-1"> 
+                                        {{ucfirst($nt->message)}}
                                     </div>
+                                    @if($nt->attechment)
                                     <div class="card-action my-2">
-                                        <a href="#" class="btn btn-soft-danger rounded-sm new-shadow-sm font-weight-bold px-3 mr-1">XYZ.txt</a>
-
-                                        <a href="#" class="btn btn-soft-success rounded-sm new-shadow-sm font-weight-bold px-3 mr-1">XYZ.pdf</a>
-                                    </div>
+                                        <a href="{{asset('attechment')}}/{{$nt->attechment}}" class="btn btn-soft-danger rounded-sm new-shadow-sm font-weight-bold px-3 mr-1">{{$nt->attechment}}</a>
+                                        
+                                    </div> 
+                                    @endif
                                </div>
-                               
                            </div>
                        </div>
+                       @endif
+                       @endforeach
+                       
                    </div>
                 </div>
             @endsection
