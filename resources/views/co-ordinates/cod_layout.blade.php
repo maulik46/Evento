daadcb1b0072e506555ebea27b56aa431b302a3d
parth177 <59960568+parth177@users.noreply.github.com>
2020-01-21 19:53:58 +0530

    setting the issue of notice display notification

--- resources/views/co-ordinates/cod_layout.blade.php
+++ resources/views/co-ordinates/cod_layout.blade.php
@@ -112,7 +112,7 @@
                         </div>
                     </li>
                       <?php
-                        $notice=\DB::table('tblnotice')->where([['receiver','coordinator'],['clgcode',Session::get('clgcode')]])->get()->toarray();
+                        $notice=\DB::table('tblnotice')->where([['receiver','coordinator'],['clgcode',Session::get('clgcode')]])->orderby('nid','desc')->get()->toarray();
                         $lastevent=App\tblcoordinaters::select('last_noti')->where('cid',Session::get('cid'))->first();
                         $count=\DB::table('tblnotice')->select('nid')->where([['nid','>',$lastevent->last_noti],['receiver','coordinator'],['clgcode',Session::get('clgcode')]])->count();
                     ?>
@@ -144,22 +144,27 @@
          <!-- inbox Right Sidebar -->
          <div class="right-bar bg-light overflow-auto my-scroll">
              <div class="rightbar-title" style="border-bottom: 1px solid #d3d3d36b;">
-                 <a href="#" class="right-bar-toggle float-right text-dark" id="close-btn">
+                 <a href="#" class="right-bar-toggle float-right text-dark" id="close-btn" onclick="window.location.reload()">
                      <i data-feather="x-circle" height="20px"></i>
                  </a>
                  <h4 class="m-0">Inbox</h4>
              </div>
 
              <div class="my-scroll px-2">
-             <?php $c=0;?>
+             <?php $c=0;           
+             ?>
             @foreach($notice as $nt)
             <?php $c++;
-                if($c=1)
+                if($c==1)
                 {
                     $lastnotice=$nt->nid;
                 }
             ?>
+            @if($c<=$count)
             <div class="card new-shadow-sm my-2 rounded-0 hover-me-sm" style="border-left: 4px solid #ff5c75;">
+            @else
+            <div class="card new-shadow-sm my-2 rounded-0 hover-me-sm" style="border-left: 4px solid #1AE1AC;">
+            @endif
                            <div class="card-body py-2">
                                <div class="d-flex justify-content-between align-items-center flex-wrap">
                                     <span class="badge badge-soft-primary px-3 py-1 badge-pill">{{date('d/m/Y',strtotime($nt->ndate))}}</span>
@@ -173,7 +178,6 @@
                                     @if($nt->attechment)
                                     <div class="card-action my-2">
                                         <a href="{{asset('attechment')}}/{{$nt->attechment}}" class="btn btn-soft-danger rounded-sm new-shadow-sm font-weight-bold px-3 mr-1">{{$nt->attechment}}</a>
-                                        
                                     </div> 
                                     @endif
                                </div>
@@ -247,7 +251,11 @@
                 })
         });
     </script>
-
+<script>
+$('.rightbar-overlay').click(function(){
+    window.location.reload();
+});
+</script>
 </body>
 
 </html>
