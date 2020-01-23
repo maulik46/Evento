6b412f6c2f42ed6d342c29afcea3c8578bdaad6d
parth177 <59960568+parth177@users.noreply.github.com>
2020-01-23 19:43:33 +0530

    Multiple file display

--- resources/views/notice.blade.php
+++ resources/views/notice.blade.php
@@ -21,55 +21,39 @@
                     ?>
                     @if($nt->nid > $new['last_noti'])
                        <div class="card new-shadow-sm my-2 rounded-0 hover-me-sm" style="border-left: 4px solid #ff5c75;">
-                           <div class="card-body py-2">
+                    @else
+                    <div class="card new-shadow-sm my-2 rounded-0 hover-me-sm" style="border-left: 4px solid #1AE1AC;">
+                    @endif
+                    <div class="card-body py-2">
                                <div class="d-flex justify-content-between align-items-center flex-wrap">
                                     <div>
-                                        <span class="badge badge-soft-primary px-3 py-1 badge-pill">{{date('d/m/Y',strtotime($nt->ndate))}}</span>
-                                        <span class="badge badge-soft-info px-3 py-1 badge-pill">12:25 pm</span>
-                                   </div>
-                                    <h6>{{ucfirst($nt->sender)}}</h6>
-                               </div>
-                            
+                                            <span class="badge badge-soft-primary px-3 py-1 badge-pill">{{date('d/m/Y',strtotime($nt->ndate))}}</span>
+                                            <span class="badge badge-soft-info px-3 py-1 badge-pill">12:25 pm</span>
+                                    </div>
+                                        <h6>{{ucfirst($nt->sender)}}</h6>
+                                </div>
                                <div>
                                     <h5 class="mt-0">{{ucfirst($nt->topic)}}</h5>
                                     <div class="card-text mb-1"> 
                                        {!! ucfirst($nt->message) !!}
                                     </div>
                                     @if($nt->attechment)
-                                    <div class="card-action my-2">
-                                        <a href="{{asset('attechment')}}/{{$nt->attechment}}" class="btn btn-soft-danger rounded-sm new-shadow-sm font-weight-bold px-3 mr-1">{{$nt->attechment}}</a>
-                                        
-                                    </div> 
+                                        <?php $att = explode('-',$nt->attechment);
+                                        $c=count($att);
+                                        $a=0;
+                                        ?>
+                                            @foreach($att as $attachment)
+                                            <?php $a++;?>
+                                            @if($a<$c)
+                                                <div class="card-action my-2">
+                                                    <a href="{{asset('attachment')}}/{{$attachment}}" class="btn btn-soft-danger rounded-sm new-shadow-sm font-weight-bold px-3 mr-1" download="{{substr($attachment, strpos($attachment, 'N') + 1)}}">{{substr($attachment, strpos($attachment, "N") + 1)}}</a> 
+                                                </div> 
+                                            @endif
+                                            @endforeach
                                     @endif
                                </div>
                            </div>
                        </div>
-                       @else
-                       <div class="card new-shadow-sm my-2 rounded-0 hover-me-sm" style="border-left: 4px solid #1AE1AC;">
-                           <div class="card-body py-2">
-                               <div class="d-flex justify-content-between align-items-center flex-wrap">
-                                    <div>
-                                        <span class="badge badge-soft-primary px-3 py-1 badge-pill">{{date('d/m/Y',strtotime($nt->ndate))}}</span>
-                                        <span class="badge badge-soft-info px-3 py-1 badge-pill">12:25 pm</span>
-                                   </div>
-                                    <h6>{{ucfirst($nt->sender)}}</h6>
-                               </div>
-                               <hr style="margin: 0.5em 0em;">
-                               <div>
-                                    <h5 class="mt-0">{{ucfirst($nt->topic)}}</h5>
-                                    <div class="card-text mb-1"> 
-                                       {!! ucfirst($nt->message) !!}
-                                    </div>
-                                    @if($nt->attechment)
-                                    <div class="card-action my-2">
-                                        <a href="{{asset('attechment')}}/{{$nt->attechment}}" class="btn btn-soft-dark rounded-sm new-shadow-sm font-weight-bold px-3 mr-1">{{$nt->attechment}}</a>
-                                        
-                                    </div> 
-                                    @endif
-                               </div>
-                           </div>
-                       </div>
-                       @endif
                        @endforeach
                        
                    </div>
