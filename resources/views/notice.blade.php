3164632678aecd9517931a2cd40d1d8e0a94e90e
maulik46 <maulikp4646@gmail.com>
2020-01-27 20:38:12 +0530

    design updated

--- resources/views/notice.blade.php
+++ resources/views/notice.blade.php
@@ -42,14 +42,16 @@
                                         $c=count($att);
                                         $a=0;
                                         ?>
+                                        <div class="card-action my-2">
                                             @foreach($att as $attachment)
                                             <?php $a++;?>
                                             @if($a<$c)
-                                                <div class="card-action my-2">
-                                                    <a href="{{asset('attachment')}}/{{$attachment}}" class="btn btn-soft-danger rounded-sm new-shadow-sm font-weight-bold px-3 mr-1" download="{{substr($attachment, strpos($attachment, 'N') + 1)}}">{{substr($attachment, strpos($attachment, "N") + 1)}}</a> 
-                                                </div> 
+                                                    <a href="{{asset('attachment')}}/{{$attachment}}" class="btn btn-soft-danger rounded-sm new-shadow-sm font-weight-bold px-3 mr-1 my-2" download="{{substr($attachment, strpos($attachment, 'N') + 1)}}">{{substr($attachment, strpos($attachment, "N") + 1)}}</a> 
+                                                    
                                             @endif
                                             @endforeach
+                                        </div> 
+                                            
                                     @endif
                                </div>
                            </div>
