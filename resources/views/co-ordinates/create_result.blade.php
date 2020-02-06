cc1a51e72ea694d1aa71452324300bc8ba701f66
maulik46 <maulikp4646@gmail.com>
2020-02-06 14:20:55 +0530

    rank page updated

--- resources/views/co-ordinates/create_result.blade.php
+++ resources/views/co-ordinates/create_result.blade.php
@@ -2,48 +2,39 @@
 
 @section('title','Create Result')
 @section('head-tag-links')
-<style>
-    .form-control {
-        border-radius: .1rem;
-        background-color: #fff !important;
-        padding: 5px 10px;
-        border: 1px solid #d1d1d1;
-        font-size: 1.1em;
-        color: #333;
-        height: 40px;
-        cursor: text !important;
-    }
-
-    .form-control:focus {
-        border: 1px solid #d1d1d190 !important;
-    }
-
-    .droppable-7 {
-        /* width: 100px; */
-        /* height: 90px;padding: 0.5em; float: left; 
-            margin: 10px; 
-            border: 1px solid black; 
-            background-color:#A39480; */
-    }
-
-    .droppable.active {
-        background-color: red;
-    }
-    body{
-        margin-bottom:1000px;
-    }
-    .sticky {
-        position: fixed;
-        top: 72px;
-        left:0px;
-        width:100%;
-        border-top:1px solid lightgray;
-        border-radius:0px!important;
-        z-index:99;
-        box-shadow: 0 3px 6px -5px #777;
+    <style>
+        .form-control {
+            border-radius: .1rem;
+            background-color: #fff !important;
+            padding: 5px 10px;
+            border: 1px solid #d1d1d1;
+            font-size: 1.1em;
+            color: #333;
+            height: 40px;
+            cursor: text !important;
         }
-        
-</style>
+        .form-control:focus {
+            border: 1px solid #d1d1d190 !important;
+        }
+        body{
+            margin-bottom:1000px;
+        }
+        .sticky {
+            position: fixed;
+            top: 72px;
+            left:0px;
+            width:100%;
+            border-top:1px solid lightgray;
+            border-radius:0px!important;
+            z-index:99;
+            box-shadow: 0 3px 6px -5px #777;
+            }
+        .stud-info:hover .drag-me{
+            -webkit-transform: translateY(-2px);
+            transform: translateY(-2px);
+            box-shadow: 0 1px 6px 0 rgba(0, 0, 0, .12), 0 1px 1px 0 rgba(0, 0, 0, .08);
+        } 
+    </style>
 <link href="{{asset('assets/libs/jquery-ui/jquery-ui.min.css')}}" rel="stylesheet">
 @endsection
 @section('my-content')
@@ -70,110 +61,87 @@
         </div>
         <hr class="my-0">
     </div>
-    <!-- <div class="card">
-        <div class="card-body">
-            <div>
-                <span class="badge badge-success px-5 mb-1">Rank 1</span>
-                <div class="card bg-soft-success p-2 px-3 droppable-rank" style="border:2px dashed #333;">
-                   <img src="{{asset('assets/images/svg-icons/student-dash/winner/1.svg')}}" height="50px" alt="1">
-                </div>
+   
 
-                <span class="badge badge-primary px-4 mb-1">Rank 2</span>
-                <div class="card bg-soft-primary p-2 px-3 droppable-rank" style="border:2px dashed #333;">
-                    <img src="{{asset('assets/images/svg-icons/student-dash/winner/2.svg')}}" height="40px" alt="2">
+    <div class="card" id="myHeader">
+        <div class="card-body py-2">
+            <div class="row align-items-center" style="margin-bottom:-15px;">
+                <div class="col-md-4">
+                    <span class="badge badge-success px-5 mb-1">Rank 1</span>
+                    <div class="card bg-soft-success p-2 px-3 droppable-rank" style="border:2px dashed #333;">
+                    <img src="{{asset('assets/images/svg-icons/student-dash/winner/1.svg')}}" height="35px" alt="1">
+                    </div>
                 </div>
-
-                <span class="badge badge-warning px-3 mb-1">Rank 3</span>
-                <div class="card bg-soft-warning p-2 px-3 droppable-rank" style="border:2px dashed #333;">
-                    <img src="{{asset('assets/images/svg-icons/student-dash/winner/3.svg')}}" height="35px" alt="3">
+                    
+                <div class="col-md-4">
+                    <span class="badge badge-primary px-4 mb-1">Rank 2</span>
+                    <div class="card bg-soft-primary p-2 px-3 droppable-rank" style="border:2px dashed #333;">
+                        <img src="{{asset('assets/images/svg-icons/student-dash/winner/2.svg')}}" height="35px" alt="2">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <span class="badge badge-warning px-3 mb-1">Rank 3</span>
+                    <div class="card bg-soft-warning p-2 px-3 droppable-rank" style="border:2px dashed #333;">
+                        <img src="{{asset('assets/images/svg-icons/student-dash/winner/3.svg')}}" height="35px" alt="3">
+                    </div>
                 </div>
             </div>
-            <div class="text-center pt-3">
-                <a href="#" class="btn btn-success px-4 new-shadow-sm rounded-sm font-weight-bold hover-me-sm">
-                Done
+            <div class="text-center">
+                <a href="#" class="btn btn-success px-4 new-shadow-sm rounded-sm font-weight-bold hover-me-sm mx-1">
+                    Done
                 <i data-feather="check-square" height="19px"></i>
                 </a>
-                <a href="#" class="btn btn-danger px-4 new-shadow-sm rounded-sm font-weight-bold hover-me-sm" onclick="window.location.reload();">
-                Clear
+                <a href="#" class="btn btn-danger px-4 new-shadow-sm rounded-sm font-weight-bold hover-me-sm mx-1" onclick="window.location.reload();">
+                    Clear
                 <i data-feather="refresh-cw" height="19px"></i>
                 </a>
             </div>
         </div>
-        
-    </div> -->
-
-    <div class="card" id="myHeader">
-    <div class="card-body">
-        <h1>rank</h1>
-    </div>
     </div>
 
-    <div class="card mb-2 pb-1 rounded-sm new-shadow-sm">
-        <div class="card-body py-2 ">
-            <div class="h5 d-flex align-items-center">
+    <div class="card mb-2 pb-2 rounded-sm new-shadow-sm" id="all-candidate">
+        <div class="card-body pt-2 pb-0 row justify-content-between">
+            <div class="h5 ml-2 d-flex align-items-center">
                 <i data-feather="users" class="icon-dual-info"></i>
                 <span class="ml-1">All Candidates</span>
             </div>
-        </div>
-        <div class="d-flex flex-wrap justify-content-between align-items-center p-2" style="margin-top:-20px;">
-            <div class="ml-2 mt-4 d-flex align-items-center">
+            <div class=" mr-2 d-flex align-items-center">
                 <span class="badge badge-soft-primary badge-pill px-3">Total</span>
                 <span class="badge badge-primary badge-pill ml-1">2</span>
             </div>
-            <div class="d-flex align-items-center col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                <input id="myInput" class="form-control" type="text" placeholder="Search Candidate Name..">
-            </div>
         </div>
-        
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <input id="myInput" class="form-control rounded" type="text" placeholder="Search Candidate Name..">
+        </div>
     </div>
 
 
     <div id="my-record">    
-        <div class="card p-1 mb-2 px-3 pb-1 hover-me-sm new-shadow-sm stud-info">
-            <div class="font-size-16 font-weight-bold text-dark">
+        <div class="card p-1 mb-2 px-3 pb-1 new-shadow-sm stud-info">
+            <div class="row p-1">
+            <div class="col-md-4 drag-me hover-me-sm rounded bg-white" style="border: 1px dashed lightgray;">
+                <div class="font-size-16 font-weight-bold text-dark">
                 Piyush Mukeshbhai Monpara
-            </div>
-            <div class="d-flex flex-wrap align-items-center justify-content-between p-1">
-                <div class="eid">
+                </div>
+                <div>
                     <span class="text-dark">EID</span>
-                    <span class="font-weight-bold mx-2">E12345677890</span>
+                    <span class="font-weight-bold mx-2 eid">E12345677890</span>
                 </div>
-                <div>
+            </div>
+            <div class="col-md-8 d-flex px-0">
+                <div class="col-sm-4 d-flex align-items-end">
                     <span class="text-dark">Class</span>
-                    <span class="font-weight-bold mx-2">TYBCA</span>
+                    <span class="font-weight-bold mx-2">Fybca</span>
                 </div>
-                <div>
+                <div class="col-sm-4 d-flex align-items-end">
                     <span class="text-dark">Division</span>
-                    <span class="font-weight-bold mx-2">3</span>
+                    <span class="font-weight-bold mx-2">1</span>
                 </div>
-                <div>
+                <div class="col-sm-4 d-flex align-items-end">
                     <span class="text-dark">Gender</span>
                     <span class="font-weight-bold mx-2">Male</span>
                 </div>
             </div>
-        </div>
-
-        <div class="card p-1 mb-2 px-3 pb-1 hover-me-sm new-shadow-sm stud-info">
-            <div class="font-size-16 font-weight-bold text-dark">
-                Pilo  Monpara
-            </div>
-            <div class="d-flex flex-wrap align-items-center justify-content-between p-1">
-                <div>
-                    <span class="text-dark">EID</span>
-                    <span class="font-weight-bold mx-2">E12345677890</span>
-                </div>
-                <div>
-                    <span class="text-dark">Class</span>
-                    <span class="font-weight-bold mx-2">TYBCA</span>
-                </div>
-                <div>
-                    <span class="text-dark">Division</span>
-                    <span class="font-weight-bold mx-2">3</span>
-                </div>
-                <div>
-                    <span class="text-dark">Gender</span>
-                    <span class="font-weight-bold mx-2">Male</span>
-                </div>
             </div>
         </div>
     </div>
@@ -200,7 +168,7 @@
 
 <script>
     $(function () {
-        $('.stud-info').draggable({
+        $('.drag-me').draggable({
             revert: true
         });
         $('.droppable-rank').droppable({
@@ -213,8 +181,12 @@
         });
     });
 
-    $('.stud-info').hover(function(){
+    $('.stud-info').mouseenter(function(){
         $(this).css("z-index","100");
+    })
+
+    $('.stud-info').mouseleave(function(){
+        $(this).css("z-index","0");
     })
 </script>
 
@@ -223,13 +195,16 @@ window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
+var allCandidate=document . getElementById("all-candidate");
 
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+    allCandidate.style.marginTop="250px";
     
   } else {
     header.classList.remove("sticky");
+    allCandidate.style.marginTop="0px";
   }
 }
 </script>
