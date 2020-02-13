24758cb4b0181a3c72e10afa54d30bb2cdc0e4ef
yash-201 <59959814+yash-201@users.noreply.github.com>
2020-02-13 16:43:55 +0530

    Email title Example to change Evento

--- app/Http/Controllers/student.php
+++ app/Http/Controllers/student.php
@@ -171,7 +171,7 @@ class student extends Controller
             $to_email=Session::get('email');
             $data=array('name'=>'OTP :'.$rand_num,'body'=>Session::get('clgname'));
             // \Mail::send('email',$data,function($message) use ($to_name,$to_email){
-            //     $message->to($to_email)
+            //     $message->to($to_email)->replyTo('eventoitsol@gmail.com',$name=null)->from('eventoitsol@gmail.com', $name = 'Evento')
             //     ->subject('Log Authentication');
             // });
             //echo "email send";
