195bc9d2e112ceead66a0333c4ac0bbdec591cbf
maulik46 <maulikp4646@gmail.com>
2020-02-18 20:52:50 +0530

    little update

--- resources/views/co-ordinates/cod_layout.blade.php
+++ resources/views/co-ordinates/cod_layout.blade.php
@@ -199,33 +199,8 @@
                            </div>
                        </div>
             @endforeach
-                 <!-- <div class="card new-shadow-sm my-2  hover-me-sm rounded-0">
-                     <div class="card-body py-2">
-                         <div class="d-flex justify-content-between align-items-center flex-wrap">
-                             <span class="badge badge-soft-primary px-3 py-1 badge-pill">28/12/2019</span>
-                             <h6>Dr.Rohit Radadiya</h6>
-                         </div>
-                         <div>
-                             <h5 class="mt-0">Picnic Notice</h5>
-                             <div class="card-text">
-                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Non tenetur possimus
-                                 adipisci?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis
-                                 explicabo itaque fugit pariatur adipisci. Quod, pariatur corrupti, laborum eius
-                                 quaerat eos vero, necessitatibus inventore nihil provident ut consequuntur
-                                 soluta magnam.
-                             </div>
-                             <div class="card-action my-2">
-                                 <a href="#"
-                                     class="btn btn-soft-success rounded-sm new-shadow-sm font-weight-bold px-3 mr-1">XYZ.txt</a>
-
-                                 <a href="#"
-                                     class="btn btn-soft-success rounded-sm new-shadow-sm font-weight-bold px-3 mr-1">XYZ.pdf</a>
-                             </div>
-                         </div>
-
-                     </div>
-                 </div> -->
-             </div>
+                
+            </div>
          </div>
          <!-- inbox Right-bar -->
 
