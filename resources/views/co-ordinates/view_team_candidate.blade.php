6a9cd0b8d79a14cfe90f93fa971965f4c44b39ad
maulik46 <maulikp4646@gmail.com>
2020-02-18 17:10:53 +0530

    page updated

--- resources/views/co-ordinates/view_team_candidate.blade.php
+++ resources/views/co-ordinates/view_team_candidate.blade.php
@@ -1,3 +1,7 @@
+<?php 
+    use App\tblstudent;
+    use App\participant;
+?>
 @extends('co-ordinates/cod_layout')
 
 @section('title','View Team Candidates')
@@ -5,11 +9,15 @@
 @section('my-content')
 <div class="container-fluid">
     <div class="mb-0 pt-2 card new-shadow-sm">
-        <a href="{{url('cindex')}}" class="text-right text-dark px-2">
+        <a href="{{url('/cindex')}}" class="text-right text-dark px-2">
             <i data-feather="x-circle" id="close-btn" height="20px"></i>
         </a>
         <span class="h3 my-0 font-weight-normal text-dark text-center">
-        Team ABC
+        Team
+        @foreach($team_candidates as $tc)
+            {{ ucfirst($tc['tname']) }}    
+        @endforeach
+        
         </span>
         <h6 class="font-weight-normal text-dark text-center">
         {{ucfirst(Session::get('clgname'))}}
@@ -21,6 +29,7 @@
             <div class="table-responsive overflow-auto my-scroll" style="height: 350px;">
                 <table class="table table-hover table-nowrap mb-0" >
                     <thead style="background-color:#1ce1ac40;color:#000;">
+                    
                         <tr>
                             <th scope="col">No</th>
                             <th scope="col">EID</th>
@@ -30,71 +39,20 @@
                         </tr>
                     </thead>
                     <tbody class="text-dark">
+                    <?php $c=0;?>
+                    @foreach($team_candidates as $tc)
+                    <?php $enrl=explode("-",$tc['senrl'])?>
+                    @foreach($enrl as $e)
+                    <?php $c++; $sinfo=tblstudent::where('senrl',$e)->first();?>
                         <tr>
-                            <th scope="row">#1</th>
-                            <td>E12345</td>
-                            <td>Piyush Monpara</td>
-                            <td>TYBCA</td>
-                            <td>3</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">#1</th>
-                            <td>E12345</td>
-                            <td>Piyush Monpara</td>
-                            <td>TYBCA</td>
-                            <td>3</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">#1</th>
-                            <td>E12345</td>
-                            <td>Piyush Monpara</td>
-                            <td>TYBCA</td>
-                            <td>3</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">#1</th>
-                            <td>E12345</td>
-                            <td>Piyush Monpara</td>
-                            <td>TYBCA</td>
-                            <td>3</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">#1</th>
-                            <td>E12345</td>
-                            <td>Piyush Monpara</td>
-                            <td>TYBCA</td>
-                            <td>3</td>
-                        </tr><tr>
-                            <th scope="row">#1</th>
-                            <td>E12345</td>
-                            <td>Piyush Monpara</td>
-                            <td>TYBCA</td>
-                            <td>3</td>
-                        </tr><tr>
-                            <th scope="row">#1</th>
-                            <td>E12345</td>
-                            <td>Piyush Monpara</td>
-                            <td>TYBCA</td>
-                            <td>3</td>
-                        </tr><tr>
-                            <th scope="row">#1</th>
-                            <td>E12345</td>
-                            <td>Piyush Monpara</td>
-                            <td>TYBCA</td>
-                            <td>3</td>
-                        </tr><tr>
-                            <th scope="row">#1</th>
-                            <td>E12345</td>
-                            <td>Piyush Monpara</td>
-                            <td>TYBCA</td>
-                            <td>3</td>
-                        </tr><tr>
-                            <th scope="row">#1</th>
-                            <td>E12345</td>
-                            <td>Piyush Monpara</td>
-                            <td>TYBCA</td>
-                            <td>3</td>
-                        </tr>             
+                            <th scope="row">{{$c}}</th>
+                            <td>{{$e}}</td>
+                            <td>{{ucfirst($sinfo['sname'])}}</td>
+                            <td>{{ucfirst($sinfo['class'])}}</td>
+                            <td>{{ucfirst($sinfo['division'])}}</td>
+                        </tr> 
+                    @endforeach
+                    @endforeach
                     </tbody>
                 </table>
             </div>
