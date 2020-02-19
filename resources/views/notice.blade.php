7168a89739d27caaf8514e2421fba1f89009ad6c
maulik46 <maulikp4646@gmail.com>
2020-02-19 14:25:42 +0530

    design changed

--- resources/views/notice.blade.php
+++ resources/views/notice.blade.php
@@ -20,37 +20,42 @@
                         }
                     ?>
                     @if($nt->nid > $new['last_noti'])
-                       <div class="card new-shadow-sm my-2 rounded-0 hover-me-sm" style="border-left: 4px solid #ff5c75;">
+                    <div class="card new-shadow-sm my-2 hover-me-sm" style="border-left: 4px solid #ff5c75;border-radius:0px 10px 10px 0px;">
                     @else
-                    <div class="card new-shadow-sm my-2 rounded-0 hover-me-sm" style="border-left: 4px solid #1AE1AC;">
+                    <div class="card new-shadow-sm my-2 hover-me-sm" style="border-left: 4px solid #1AE1AC;border-radius:0px 10px 10px 0px;">
                     @endif
-                    <div class="card-body py-2">
-                               <div class="d-flex justify-content-between align-items-center flex-wrap">
-                                    <div>
-                                            <span class="badge badge-soft-primary px-3 py-1 badge-pill">{{date('d/m/Y',strtotime($nt->ndate))}}</span>
-                                            <span class="badge badge-soft-info px-3 py-1 badge-pill">{{date('h:i A',strtotime($nt->ntime))}}</span>
-                                    </div>
-                                        <h6>{{ucfirst($nt->sender)}}</h6>
+                    <div class="card-body py-0">
+                            <div class="d-flex justify-content-between align-items-center flex-wrap">
+                                <div style="margin-top:-10px;">
+                                    <span class="badge badge-soft-primary px-3 py-1 badge-pill">{{date('d/m/Y',strtotime($nt->ndate))}}</span>
+                                    <span class="badge badge-soft-info px-3 py-1 badge-pill">{{date('h:i A',strtotime($nt->ntime))}}</span>
                                 </div>
-                               <div>
-                                    <h5 class="mt-0">{{ucfirst($nt->topic)}}</h5>
-                                    <div class="card-text mb-1"> 
+                                <div class="d-flex align-items-end flex-column flex-wrap">
+                                    <h6>{{ucfirst($nt->sender)}}</h6>
+                                    <span class="badge badge-warning text-white badge-pill" style="padding: 0.3em 0.6rem">Co-ordinator</span>
+                                </div>
+                                    
+                            </div>
+                            <div>
+                                <h5 style="margin-top:-12px;">{{ucfirst($nt->topic)}}</h5>
+                                <div class="card-text mb-2"> 
                                        {!! ucfirst($nt->message) !!}
-                                    </div>
+                                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere vitae totam consequuntur, rerum iure corrupti? Quod, pariatur consequuntur nulla veritatis quae molestias sit est iure corrupti sequi, dolorem nesciunt consectetur!
+                                </div>
                                     @if($nt->attechment)
                                         <?php $att = explode('-',$nt->attechment);
                                         $c=count($att);
                                         $a=0;
                                         ?>
                                            
-                                                <div class="card-action my-2">
+                                        <div class="card-action my-2">
                                             @foreach($att as $attachment)
                                             <?php $a++;?>
                                             @if($a<$c)
-                                                    <a href="{{asset('attachment')}}/{{$attachment}}" class="btn btn-soft-danger rounded-sm new-shadow-sm font-weight-bold px-3 mr-1" download="{{substr($attachment, strpos($attachment, 'N') + 1)}}">{{substr($attachment, strpos($attachment, "N") + 1)}}</a> 
+                                                    <a href="{{asset('attachment')}}/{{$attachment}}" class="btn badge badge-info badge-pill p-2 new-shadow-sm font-weight-bold px-3 mr-1" download="{{substr($attachment, strpos($attachment, 'N') + 1)}}">{{substr($attachment, strpos($attachment, "N") + 1)}}</a> 
                                             @endif
                                             @endforeach    
-                                                </div> 
+                                        </div> 
                                             
                                     @endif
                                </div>
