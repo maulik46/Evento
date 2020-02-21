527683aeb46d53aea6f2e78c36b2002c95e1a1f8
maulik46 <maulikp4646@gmail.com>
2020-02-21 17:23:36 +0530

    name change

--- resources/views/super-admin/s_admin_layout.blade.php
+++ resources/views/super-admin/s_admin_layout.blade.php
@@ -94,7 +94,7 @@
                             <div class="dropdown-divider d-sm-none d-md-none"></div>
                             <a href="{{url('sindex')}}" class="dropdown-item notify-item mb-2">
                                 <i data-feather="home" class="icon-dual-primary icon-xs mr-2"></i>
-                                <span>Home</span>
+                                <span>Dashboard</span>
                             </a>
                             <a href="{{url('new_cod')}}" class="dropdown-item notify-item my-2">
                                 <i data-feather="user-plus" class="icon-dual-success icon-xs mr-2"></i>
@@ -170,9 +170,9 @@
             ?>
             @if($c<=$count) 
             <div class="card new-shadow-sm my-2 hover-me-sm"
-                style="border-left: 4px solid #ff5c75;border-radius:0px 10px 10px 0px;">
+                style="border-left: 3px solid #ff5c75;border-radius:0px 10px 10px 0px;">
                 @else
-                <div class="card new-shadow-sm my-2 hover-me-sm" style="border-left: 4px solid #1AE1AC;border-radius:0px 10px 10px 0px;">
+                <div class="card new-shadow-sm my-2 hover-me-sm" style="border-left: 3px solid #1AE1AC;border-radius:0px 10px 10px 0px;">
                     @endif
                     <div class="card-body py-0 px-2">
                         <div class="d-flex justify-content-between align-items-center flex-wrap" style="margin:-10px 0px;">
