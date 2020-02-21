b5c33054b8071694fed63dd29d6fa9372f72db9d
maulik46 <maulikp4646@gmail.com>
2020-02-21 14:23:16 +0530

    notice updated

--- resources/views/super-admin/s_admin_layout.blade.php
+++ resources/views/super-admin/s_admin_layout.blade.php
@@ -174,7 +174,7 @@
                 @else
                 <div class="card new-shadow-sm my-2 hover-me-sm" style="border-left: 4px solid #1AE1AC;border-radius:0px 10px 10px 0px;">
                     @endif
-                    <div class="card-body py-0" >
+                    <div class="card-body py-0 px-2">
                         <div class="d-flex justify-content-between align-items-center flex-wrap" style="margin:-10px 0px;">
                             <span
                                 class="badge badge-soft-primary px-3 py-1 badge-pill">{{date('d/m/Y',strtotime($nt->ndate))}}
@@ -186,7 +186,7 @@
                         </div>
                         <div>
                             <h5 class="mt-0">{{ucfirst($nt->topic)}}</h5>
-                            <div class="card-text mb-2">
+                            <div class="card-text mb-2 rounded p-2" style="border:1px solid #d3d3d369;">
                                 {!! ucfirst($nt->message) !!}
                             </div>
                             @if($nt->attechment)
