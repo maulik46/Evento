cb755e0b16ec3e4236899777dd2077165f9600b5
parth177 <59960568+parth177@users.noreply.github.com>
2020-02-22 20:27:51 +0530

    codding add

--- resources/views/super-admin/approval.blade.php
+++ resources/views/super-admin/approval.blade.php
@@ -28,17 +28,18 @@
             <span class="h4 text-dark ml-2">Approve Deleted Event</span>
         </div>
     </div>
+    @foreach($delevnt as $del)
     <div class="card mb-0 mt-1 new-shadow-sm">
         <div class="card-body py-2">
            
             <div class="row justify-content-between align-items-center mx-0">
-                <span class="text-danger font-weight-bold font-size-16">PL/SQL competition</span>
+                <span class="text-danger font-weight-bold font-size-16">{{ucfirst($del['ename'])}} competition</span>
                 <div id="labels">
                     <span class="badge badge-primary px-3 badge-pill  my-1 rounded-0" style="margin-right:-5px;">
                        Event-date
                     </span>
                     <span class="badge badge-soft-primary px-3 badge-pill  my-1 rounded-0">
-                        12/02/2020
+                        {{date('d-m-Y',strtotime($del['edate']))}}
                     </span>
                 </div>
             </div>
@@ -46,21 +47,21 @@
             <div class="row justify-content-between align-items-end">
                 <div class="card-text col-sm-9 col-12">
                     <div>
-                        <span>EventCo-ordinator</span>  Dr.Parth Patthar
+                        <span>Event Co-ordinator</span>  {{ucfirst($del['cname'])}}
                     </div>
                     <div>
-                        <span>Event-Type</span>  IT
+                        <span>Event-Type</span>  {{ucfirst($del['category'])}}
                     </div>
                     <div>
-                        <span>Reason</span> Due to emergency of world war 3 & lack resource of chips
+                        <span>Reason</span> {{ucfirst($del['reason'])}}
                     </div>
                     
-                </div>
+                </div>  
                 <div class="col-sm-3 col-12">
-                    <a href="#" class="m-1 btn btn-default btn-sm  btn-rounded float-right font-weight-bold px-4 text-danger" style="border:2px solid var(--danger);">
+                    <a href="#" onclick="return confirm('<?php echo ucfirst($del['ename'])?>','<?php echo encrypt($del['eid'])?>','cal')" class="m-1 btn btn-default btn-sm  btn-rounded float-right font-weight-bold px-4 text-danger" style="border:2px solid var(--danger);">
                         <span>Cancel</span>    
                     </a>
-                    <a href="#" class="m-1 btn btn-danger btn-sm  btn-rounded float-right font-weight-bold px-4" >
+                    <a href="" onclick="return confirm('<?php echo ucfirst($del['ename'])?>','<?php echo encrypt($del['eid'])?>','del')" class="m-1 btn btn-danger btn-sm  btn-rounded float-right font-weight-bold px-4" >
                         <span>Delete</span>    
                     </a>
                     
@@ -68,9 +69,41 @@
             </div>
         </div>
     </div>
+    @endforeach
 </div>
 @endsection
 @section('extra-scripts')
-
+<script src="{{asset('assets/js/sweetalert2.min.js')}}"></script>
+<script>
+    function confirm(ename,eid,val)
+    {
+        var y=0;
+        if(val=="del")
+        {
+            var msg= "You won't delete Event <b>" + ename + " </b> !!";
+            var btn='Yes, delete it!';
+        }
+        else
+        {
+            var msg= "You won't reject request for delete Event <b>" + ename + " </b> !!";
+            var btn='Yes, cancle it!';
+        }
+        Swal.fire({
+        title: 'Are you sure?',
+        html:msg,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: btn,
+        }).then((result) => {
+        if (result.value) {
+            window.location.href = '<?php echo url('/approval/confrim_del').'/' ?>'+eid+'/'+val;
+        }
+        })
+        
+        return false;
+    }
+</script>
 
 @endsection
