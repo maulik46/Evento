491a3feab520b8fd978620061ee22f4e4b608998
parth177 <59960568+parth177@users.noreply.github.com>
2020-02-29 18:01:54 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -196,7 +196,7 @@ Route::group(['middleware' => 'admin_session_check'], function () {
 
           route::get('/check_logs', function(){
             $logs=log::join('tblcoordinaters','tblcoordinaters.cid','tbllog.uid')
-            ->where([['tblcoordinaters.clgcode',Session::get('clgcode')],['tbllog.utype','co_ordinatore']])
+            ->where([['tblcoordinaters.clgcode',Session::get('clgcode')],['tbllog.utype','co-ordinatore']])
             ->orderby('time','desc')->paginate(10);
             $cod=App\tblcoordinaters::select('cid','cname')->where('clgcode',Session::get('clgcode'))->get()->toarray();
             return view('super-admin/check_logs',['logs'=>$logs,'cod'=>$cod]);
