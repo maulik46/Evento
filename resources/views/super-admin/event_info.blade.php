a2c4629825587c6f34ff517671b72c4d4759d4d3
maulik46 <maulikp4646@gmail.com>
2020-03-04 17:24:11 +0530

    page updated 2

---
+++ resources/views/super-admin/event_info.blade.php
@@ -0,0 +1,95 @@
+@extends('super-admin/s_admin_layout')
+@section('title','Event Information')
+@section('head-tag-links')
+<style>
+    .printer{
+        visibility:hidden;
+    }
+    .print-btn:hover .printer,
+    .card:hover .printer{
+        visibility:visible;
+    }
+    .card:hover .print-btn,
+    .print-btn:hover{
+        width:55px!important;
+        padding:0.5rem 0.9rem!important;
+        transition:0.2s ease-out;
+    }
+</style>
+@endsection
+@section('my-content')
+            <div class="d-flex justify-content-center">
+                <div class="container-fluid col-lg-6 col-md-9 col-sm-10">
+                  <div class="card new-shadow-2 rounded">
+                        <a href="{{url('sindex')}}" class="text-right text-dark p-2">
+                            <i data-feather="x-circle" id="close-btn" height="20px"></i>
+                        </a>
+                        <div class="position-absolute print-btn bg-success new-shaow-sm" style="padding:0.5rem 0.3rem;top: 15%;left:100%;width:0px;border-radius:0px 5px 5px 0px;" data-toggle="tooltip" data-placement="left" title="Print">
+                            <a href="#" class="printer">
+                                <i data-feather="printer" class="text-white"></i>
+                            </a>
+                        </div>
+                        <div class="card-body px-2">
+                            <div class="text-center">
+                              <h2 class="font-weight-light"> {{ucfirst($einfo['ename'])}}</h2>
+                             <span>{{ucfirst(Session::get('clgname'))}}</span>
+                             <p class="my-2">
+                                 <span class="font-weight-bold">{{ucfirst(Session::get('cname'))}}</span>(Co-ordinator)
+                            </p>
+                            </div>
+                            <hr>
+                            <div id="event-info" class="p-1  text-dark font-size-15">
+                                <p class="d-flex justify-content-between mx-5">
+                                    <span class="font-weight-bold">Event Starting date</span>
+                                    <span>{{date('d/m/Y',strtotime($einfo['edate']))}}</span>
+                                </p>
+                                <p class="d-flex justify-content-between mx-5">
+                                    <span class="font-weight-bold">Event Ending date</span>
+                                    <span>{{date('d/m/Y',strtotime($einfo['enddate']))}}</span>
+                                </p>
+                                <p class="d-flex justify-content-between mx-5">
+                                    <span class="font-weight-bold text-right">Event time</span>
+                                    <span>{{date('h:i A',strtotime($einfo['etime']))}}</span>
+                                </p>
+                                 <p class="d-flex justify-content-between mx-5">
+                                    <span class="font-weight-bold">Registraion start date</span>
+                                    <span>{{date('d/m/Y',strtotime($einfo['reg_start_date']))}}</span>
+                                </p>
+                                <p class="d-flex justify-content-between mx-5">
+                                    <span class="font-weight-bold">Registraion last date</span>
+                                    <span>{{date('d/m/Y',strtotime($einfo['reg_end_date']))}}</span>
+                                </p>
+                                <p class="d-flex justify-content-between mx-5">
+                                    <span class="font-weight-bold">Event Type</span>
+                                    <span>{{ucfirst($einfo['e_type'])}}</span>
+                                </p>
+                                @if($einfo['e_type']=="team")
+                                <p class="d-flex justify-content-between mx-5">
+                                    <span class="font-weight-bold">Team Size</span>
+                                    <span>{{$einfo['tsize']}}</span>
+                                </p>
+                                @endif
+                                <p class="d-flex justify-content-between mx-5">
+                                    <span class="font-weight-bold">Event Catagory</span>
+                                    <span>{{ucfirst($einfo['category'])}}</span>
+                                </p>
+                                <p class="d-flex justify-content-between mx-5">
+                                    <span class="font-weight-bold">Event For</span>
+                                    <span>{{ucfirst($einfo['gallow'])}}</span>
+                                </p>
+                                
+                                <p class="d-flex justify-content-between mx-5">
+                                    <span class="font-weight-bold">Event Location</span>
+                                    <span>{{ucfirst($einfo['place'])}}</span>
+                                </p>
+                            </div>
+                        
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+@endsection
+
+
---
+++ resources/views/super-admin/view_result.blade.php
@@ -0,0 +1,148 @@
+<?php
+    use \App\Http\Controllers\co_ordinate;
+?>
+@extends('super-admin/s_admin_layout')
+
+@section('title','View Result')
+
+@section('my-content')
+<div class="container-fluid">
+    <div class="mb-0 pt-2 card new-shadow-sm">
+         <a href="{{url('sindex')}}" class="text-right text-dark px-2">
+            <i data-feather="x-circle" id="close-btn" height="20px"></i>
+        </a> 
+        <h2 class="font-weight-normal text-dark text-center">{{ucfirst($einfo['ename'])}}</h2>
+        <h6 class="font-weight-normal text-dark text-center">{{ucfirst(Session::get('clgname'))}}</h6>
+        <h6 class="font-weight-normal text-dark text-center">
+            <span class="font-weight-bold badge badge-soft-dark px-3 badge-pill">{{date('d/m/Y',strtotime($einfo['edate']))}}</span>
+            <span class="ml-1 font-weight-bold  badge badge-soft-dark px-4 badge-pill">{{date('l',strtotime($einfo['edate']))}}</span>
+        </h6>
+        <hr class=" my-1">
+    </div>
+<div class="mt-0">
+    <div class="card mb-0 rounded-sm">
+        <div class="card-body py-2">
+            <div class="h5 d-flex align-items-center">
+                <i data-feather="award" class="icon-dual-success"></i>
+                <span class="ml-1">Top 3 Winner Candidate</span>
+            </div>
+        </div>
+        <hr class=" my-1">
+    </div>
+    
+    <div class="card new-shadow-sm" style="max-height: 350px;">
+        <div class="card-body overflow-auto my-scroll">
+            <div class="table-responsive overflow-auto my-scroll">
+                <table class="table table-hover table-nowrap mb-0">
+                    <thead style="background-color:#1ce1ac40;color:#000;">
+                                        <tr>
+                                            <th scope="col">Rank</th>
+                                            <th scope="col">EID</th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Class</th>
+                                            <th scope="col">Division</th>
+                                        </tr>
+                                    </thead>
+                                    <?php
+                                        $r1=\DB::table('tblparticipant')->select('senrl')->where([['eid',$einfo['eid']],['rank',1]])->first();
+                                        $sinfo=co_ordinate::studinfo($r1->senrl);
+                                    ?>
+                                    <tbody class="text-dark">
+                                        <tr>
+                                            <th scope="row">
+                                                <img src="{{asset('assets/images/svg-icons/student-dash/winner/1.svg')}}" height="25px" alt="1">
+                                            </th>
+                                            <td>{{$r1->senrl}}</td>
+                                            <td>{{ucfirst($sinfo['sname'])}}</td>
+                                            <td>{{ucfirst($sinfo['class'])}}</td>
+                                            <td>{{$sinfo['division']}}</td>
+                                        </tr>
+                                    <?php
+                                        $r2=\DB::table('tblparticipant')->select('senrl')->where([['eid',$einfo['eid']],['rank',2]])->first();
+                                    ?>
+                                        @if($r2)
+                                        <?php
+                                            $sinfo=co_ordinate::studinfo($r2->senrl);
+                                        ?>
+                                        <tr>
+                                            <th scope="row">
+                                                <img src="{{asset('assets/images/svg-icons/student-dash/winner/2.svg')}}" height="25px" alt="2">
+                                            </th>
+                                            <td>{{$r2->senrl}}</td>
+                                            <td>{{ucfirst($sinfo['sname'])}}</td>
+                                            <td>{{ucfirst($sinfo['class'])}}</td>
+                                            <td>{{$sinfo['division']}}</td>
+                                        </tr>
+                                        @endif
+                                        <?php
+                                        $r3=\DB::table('tblparticipant')->select('senrl')->where([['eid',$einfo['eid']],['rank',3]])->first();
+                                        ?>
+                                        @if($r3)
+                                        <?php
+                                            $sinfo=co_ordinate::studinfo($r3->senrl);
+                                        ?>
+                                        <tr>
+                                            <th scope="row">
+                                                <img src="{{asset('assets/images/svg-icons/student-dash/winner/2.svg')}}" height="25px" alt="2">
+                                            </th>
+                                            <td>{{$r3->senrl}}</td>
+                                            <td>{{ucfirst($sinfo['sname'])}}</td>
+                                            <td>{{ucfirst($sinfo['class'])}}</td>
+                                            <td>{{$sinfo['division']}}</td>
+                                        </tr>
+                                        @endif
+                                </tbody>
+                        </table>
+                </div>
+        </div>
+    </div>
+                        
+</div>
+@if($participant > 0)
+    <div class="card mb-0 rounded-sm">
+        <div class="card-body py-2 d-flex justify-content-between align-items-center">
+            <div class="h5 d-flex align-items-center">
+                <i data-feather="users" class="icon-dual-info"></i>
+                <span class="ml-1">Other Candidates</span>
+            </div>
+            <div class="h6 d-flex align-items-center">
+                <span>Total</span>
+                <span class="ml-2">{{$participant}}</span>
+            </div>
+        </div>
+        <hr class=" my-1">
+    </div>
+    <?php
+        $parti=co_ordinate::participant($einfo['eid']);
+    ?>
+    <div class="card new-shadow-sm" style="max-height: 350px;">
+        <div class="card-body overflow-auto my-scroll">
+            <div class="table-responsive overflow-auto my-scroll">
+                <table class="table table-hover table-nowrap mb-0">
+                    <thead style="background-color:#25c2e340;color:#000;">
+                        <tr>
+                            <th scope="col">EID</th>
+                            <th scope="col">Name</t h>
+                            <th scope="col">Class</th>
+                            <th scope="col">Division</th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-dark">
+                    @foreach($parti as $participant)
+                    <?php $sinfo=co_ordinate::studinfo($participant['senrl']);?>
+                        <tr>
+                            <td>{{$participant['senrl']}}</td>
+                            <td>{{ucfirst($sinfo['sname'])}}</td>
+                            <td>{{ucfirst($sinfo['class'])}}</td>
+                            <td>{{$sinfo['division']}}</td>
+
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div> <!-- end table-responsive-->
+        </div> <!-- end card-body-->
+    </div>
+@endif
+</div>
+@endsection
