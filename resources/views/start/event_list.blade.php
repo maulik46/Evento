b969b5138ea11303de4617a028c8781902cf93e0
parth177 <59960568+parth177@users.noreply.github.com>
2020-03-06 19:00:42 +0530

    Update event_list.blade.php

--- resources/views/start/event_list.blade.php
+++ resources/views/start/event_list.blade.php
@@ -10,129 +10,50 @@
         <div>
             <div class="card new-shadow-2">
                 <h5 class="text-center mb-4 mt-2 p-2">
-                    Sutex Bank College Of Computer Applications & Science
+                <?php $clg=\DB::table('tblcolleges')->select('clgname')->where('clgcode',$clgcode)->first()?>
+                   {{ucfirst($clg->clgname)}}
                 </h5>
                 <div class="card-body p-2">
                     <h4 class="ml-2">Recent Events</h4>
                     <hr class="mt-1">
                     <div class="row owl-carousel owl-theme mx-0">
-                        <div class="col-md-12 item">
-                            <div class="card new-shadow-sm rounded-sm text-dark" style="background-color: #83d7e93d;">
-                                <div class="d-flex justify-content-between align-items-center px-3" style="background-color: #a3e5f3!important;">
-                                <h5 class="text-center">Rangoli making competition</h5>
-                                </div>
-                                <div class="card-text p-2 px-3">
-                                    <div class="row mx-0 justify-content-between">
-                                        <span class="font-weight-bold">Date</span>
-                                        <span class="text-dark">12/03/2020</span>
+                    <?php $a=0?>
+                    @foreach($events as $event)
+                    <?php $a++?>
+                        @if($a <= 4)    
+                            <div class="col-md-12 item">
+                                <div class="card new-shadow-sm rounded-sm text-dark" style="background-color: #83d7e93d;">
+                                    <div class="d-flex justify-content-between align-items-center px-3" style="background-color: #a3e5f3!important;">
+                                    <h5 class="text-center">{{ucfirst($event['ename'])}}</h5>
                                     </div>
-                                    <div class="row mx-0 justify-content-between">
-                                        <span class="font-weight-bold">Time</span>
-                                        <span class="text-dark">10:45 AM</span>
+                                    <div class="card-text p-2 px-3">
+                                        <div class="row mx-0 justify-content-between">
+                                            <span class="font-weight-bold">Date</span>
+                                            <span class="text-dark">{{date('d/m/Y',strtotime($event['edate']))}}</span>
+                                        </div>
+                                        <div class="row mx-0 justify-content-between">
+                                            <span class="font-weight-bold">Time</span>
+                                            <span class="text-dark">{{date('h:i A',strtotime($event['time']))}}</span>
+                                        </div>
+                                        <div class="row mx-0 justify-content-between align-items-center">
+                                            <span class="font-weight-bold">Type</span>
+                                            <span class="text-dark badge badge-soft-primary badge-pill px-3">{{ucfirst($event['e_type'])}}</span>
+                                        </div>
                                     </div>
-                                    <div class="row mx-0 justify-content-between align-items-center">
-                                        <span class="font-weight-bold">Type</span>
-                                        <span class="text-dark badge badge-soft-primary badge-pill px-3">Team</span>
+                                    <div class="d-flex justify-content-between align-items-center px-3 py-1">
+                                        <a href="{{url('e_info')}}/{{encrypt($event['eid'])}}" class="hover-me-sm badge badge-primary badge-pill px-3">
+                                            View Details
+                                        </a>
+                                        <a href="{{url('/index')}}" data-toggle="tooltip" title="Participate Now">
+                                            <i data-feather="arrow-right-circle" class="icon-dual-primary" height="20px"></i>
+                                        </a>
                                     </div>
-                                </div>
-                                <div class="d-flex justify-content-between align-items-center px-3 py-1">
-                                    <a href="#" class="hover-me-sm badge badge-primary badge-pill px-3">
-                                        View Details
-                                    </a>
-                                    <a href="#" data-toggle="tooltip" title="Participate Now">
-                                        <i data-feather="arrow-right-circle" class="icon-dual-primary" height="20px"></i>
-                                    </a>
-                                </div>
-                                
-                            </div>
-                        </div>
-                        <div class="col-md-12 item">
-                            <div class="card new-shadow-sm rounded-sm text-dark" style="background-color: #83d7e93d;">
-                                <div class="d-flex justify-content-between align-items-center px-3" style="background-color: #a3e5f3!important;">
-                                    <h5 class="text-center">IT Quiz competition</h5>
-                                </div>
-                                <div class="card-text p-2 px-3">
-                                    <div class="row mx-0 justify-content-between">
-                                        <span class="font-weight-bold">Date</span>
-                                        <span class="text-dark">12/03/2020</span>
-                                    </div>
-                                    <div class="row mx-0 justify-content-between">
-                                        <span class="font-weight-bold">Time</span>
-                                        <span class="text-dark">10:45 AM</span>
-                                    </div>
-                                    <div class="row mx-0 justify-content-between align-items-center">
-                                        <span class="font-weight-bold">Type</span>
-                                        <span class="text-dark badge badge-soft-primary badge-pill px-3">Team</span>
-                                    </div>
-                                </div>
-                                <div class="d-flex justify-content-between align-items-center px-3 py-1">
-                                    <a href="#" class="hover-me-sm badge badge-primary badge-pill px-3">
-                                        View Details
-                                    </a>
-                                    <a href="#" data-toggle="tooltip" title="Participate Now">
-                                        <i data-feather="arrow-right-circle" class="icon-dual-primary" height="20px"></i>
-                                    </a>
+                                    
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-md-12 item">
-                            <div class="card new-shadow-sm rounded-sm text-dark" style="background-color: #83d7e93d;">
-                                <div class="d-flex justify-content-between align-items-center px-3" style="background-color: #a3e5f3!important;">
-                                    <h5 class="text-center">PHP &web designing competition</h5>
-                                </div>
-                                <div class="card-text p-2 px-3">
-                                    <div class="row mx-0 justify-content-between">
-                                        <span class="font-weight-bold">Date</span>
-                                        <span class="text-dark">12/03/2020</span>
-                                    </div>
-                                    <div class="row mx-0 justify-content-between">
-                                        <span class="font-weight-bold">Time</span>
-                                        <span class="text-dark">10:45 AM</span>
-                                    </div>
-                                    <div class="row mx-0 justify-content-between align-items-center">
-                                        <span class="font-weight-bold">Type</span>
-                                        <span class="text-dark badge badge-soft-primary badge-pill px-3">Team</span>
-                                    </div>
-                                </div>
-                                <div class="d-flex justify-content-between align-items-center px-3 py-1">
-                                    <a href="#" class="hover-me-sm badge badge-primary badge-pill px-3">
-                                        View Details
-                                    </a>
-                                    <a href="#" data-toggle="tooltip" title="Participate Now">
-                                        <i data-feather="arrow-right-circle" class="icon-dual-primary" height="20px"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-12 item">
-                            <div class="card new-shadow-sm rounded-sm text-dark" style="background-color: #83d7e93d;">
-                                <div class="d-flex justify-content-between align-items-center px-3"style="background-color: #a3e5f3!important;">
-                                    <h5 class="text-center">Kho-Kho competition</h5>
-                                </div>
-                                <div class="card-text p-2 px-3">
-                                    <div class="row mx-0 justify-content-between">
-                                        <span class="font-weight-bold">Date</span>
-                                        <span class="text-dark">12/03/2020</span>
-                                    </div>
-                                    <div class="row mx-0 justify-content-between">
-                                        <span class="font-weight-bold">Time</span>
-                                        <span class="text-dark">10:45 AM</span>
-                                    </div>
-                                    <div class="row mx-0 justify-content-between align-items-center">
-                                        <span class="font-weight-bold">Type</span>
-                                        <span class="text-dark badge badge-soft-primary badge-pill px-3">Team</span>
-                                    </div>
-                                </div>
-                                <div class="d-flex justify-content-between align-items-center px-3 py-1">
-                                    <a href="#" class="hover-me-sm badge badge-primary badge-pill px-3">
-                                        View Details
-                                    </a>
-                                    <a href="#" data-toggle="tooltip" title="Participate Now">
-                                        <i data-feather="arrow-right-circle" class="icon-dual-primary" height="20px"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
+                        @endif
+                    @endforeach
+                      
                     </div>
                 </div>
             </div>
@@ -148,33 +69,44 @@
                     <h4 class="ml-2">Other Events</h4>
                     <hr>
                     <div class="row mx-0 justify-content-between" id="other-events">
-                        <div class="col-lg-6 col-12 px-0 card bg-light new-shadow-sm  rounded-sm text-dark hover-me-sm event-card">
-                            <div class="d-flex justify-content-between align-items-center px-3" style="background-color: #60ffd475!important">
-                                <h5 class="text-center text-dark">Rangoli making competition</h5>
-                                <a href="#" class="mr-1" data-toggle="tooltip" title="View Details">
-                                    <i data-feather="info" class="text-dark" height="20px"></i>
+                    <?php $a=0?>
+                    @foreach($events as $event)
+                    <?php $a++?>
+                        @if($a > 4)   
+                        <div class="col-lg-6 col-12">
+                            <div class=" px-0 card bg-light new-shadow-sm  rounded-sm text-dark hover-me-sm event-card">
+                                <div class="d-flex justify-content-between align-items-center px-3" style="background-color: #60ffd475!important">
+                                    <h5 class="text-center text-dark">{{ucfirst($event['ename'])}}</h5>
+                                    <a href="{{url('e_info')}}/{{encrypt($event['eid'])}}" class="mr-1" data-toggle="tooltip" title="View Details">
+                                        <i data-feather="info" class="text-dark" height="20px"></i>
+                                    </a>
+                                </div>
+                                <div class="card-text p-2 px-3">
+                                    <div class="row mx-0 justify-content-between">
+                                        <span class="font-weight-bold">Date</span>
+                                        <span class="text-dark">{{date('d/m/Y',strtotime($event['edate']))}}</span>
+                                    </div>
+                                    <div class="row mx-0 justify-content-between">
+                                        <span class="font-weight-bold">Time</span>
+                                        <span class="text-dark">{{date('h:i A',strtotime($event['time']))}}</span>
+                                    </div>
+                                    <div class="row mx-0 justify-content-between align-items-center">
+                                        <span class="font-weight-bold">Type</span>
+                                        <span class="text-dark badge badge-soft-primary badge-pill px-3">{{ucfirst($event['e_type'])}}</span>
+                                    </div>
+                                </div>
+                                <a href="{{url('/index')}}" class="participate btn btn-sm rounded-0"  style="border-top:1px solid #e9e9e9;">
+                                    <span class="text-dark font-size-13 font-weight-bold">Participate Now</span>
+                                    <i data-feather="arrow-right-circle" class="text-success" height="20px"></i>
                                 </a>
-                            </div>
-                            <div class="card-text p-2 px-3">
-                                <div class="row mx-0 justify-content-between">
-                                    <span class="font-weight-bold">Date</span>
-                                    <span class="text-dark">12/03/2020</span>
-                                </div>
-                                <div class="row mx-0 justify-content-between">
-                                    <span class="font-weight-bold">Time</span>
-                                    <span class="text-dark">10:45 AM</span>
-                                </div>
-                                <div class="row mx-0 justify-content-between align-items-center">
-                                    <span class="font-weight-bold">Type</span>
-                                    <span class="text-dark badge badge-soft-primary badge-pill px-3">Team</span>
-                                </div>
-                            </div>
-                            <a href="#" class="participate btn btn-sm rounded-0"  style="border-top:1px solid #e9e9e9;">
-                                <span class="text-dark font-size-13 font-weight-bold">Participate Now</span>
-                                <i data-feather="arrow-right-circle" class="text-success" height="20px"></i>
-                            </a>
 
+                            </div>
                         </div>
+                        @endif
+                    @endforeach
+                    @if($a <= 4)
+                        <div class="text-center">No other events available </div>
+                    @endif
                     </div>
                 </div>
             </div>
