6d8db8a24f69eed002751383615a446ce6e97ea7
parth177 <59960568+parth177@users.noreply.github.com>
2020-03-06 18:59:58 +0530

    Update e_info.blade.php

--- resources/views/start/e_info.blade.php
+++ resources/views/start/e_info.blade.php
@@ -10,57 +10,61 @@
                         </a>
                         <div class="card-body pt-0 px-2">
                             <div class="text-center">
-                              <h2 class="font-weight-light">Cricket Competition</h2>
-                             <span>{{ucfirst(Session::get('clgname'))}}</span>
+                              <h2 class="font-weight-light">{{ucfirst($einfo['ename'])}}</h2>
+                             <span>{{ucfirst($einfo['clgname'])}}</span>
                              <p class="my-2">
                                  <span class="font-weight-bold">
-                                 Yash Parmar</span>(Co-ordinator)
+                                 {{ucfirst($einfo['cname'])}}</span>(Co-ordinator)
                             </p>
                             </div>
                             <hr>
                             <div id="event-info" class="p-1  text-dark font-size-15">
                                 <p class="d-flex justify-content-between mx-5">
                                     <span class="font-weight-bold">Event Starting date</span>
-                                    <span>12/02/2020</span>
+                                    <span>{{date('d/m/Y',strtotime($einfo['edate']))}}</span>
                                 </p>
                                 <p class="d-flex justify-content-between mx-5">
                                     <span class="font-weight-bold">Event Ending date</span>
-                                    <span>15/02/2020</span>
+                                    <span>{{date('d/m/Y',strtotime($einfo['enddate']))}}</span>
                                 </p>
                                 <p class="d-flex justify-content-between mx-5">
                                     <span class="font-weight-bold text-right">Event time</span>
-                                    <span>10:30 AM</span>
+                                    <span>{{date('h:i A',strtotime($einfo['time']))}}</span>
                                 </p>
                                  <p class="d-flex justify-content-between mx-5">
                                     <span class="font-weight-bold">Registraion start date</span>
-                                    <span>02/02/2020</span>
+                                    <span>{{date('d/m/Y',strtotime($einfo['reg_start_date']))}}</span>
                                 </p>
                                 <p class="d-flex justify-content-between mx-5">
                                     <span class="font-weight-bold">Registraion last date</span>
-                                    <span>08/02/2020</span>
+                                    <span>{{date('d/m/Y',strtotime($einfo['reg_end_date']))}}</span>
                                 </p>
                                 <p class="d-flex justify-content-between mx-5">
                                     <span class="font-weight-bold">Event Type</span>
-                                    <span>Team</span>
+                                    <span>{{ucfirst($einfo['e_type'])}}</span>
                                 </p>
-                                
+                                @if($einfo['e_type']=="team")
                                 <p class="d-flex justify-content-between mx-5">
                                     <span class="font-weight-bold">Team Size</span>
-                                    <span>12</span>
+                                    <span>{{$einfo['tsize']}}</span>
                                 </p>
-                                
+                                @endif
                                 <p class="d-flex justify-content-between mx-5">
                                     <span class="font-weight-bold">Event Catagory</span>
-                                    <span>Sport</span>
+                                    <span>{{strtoupper($einfo['category'])}}</span>
                                 </p>
                                 <p class="d-flex justify-content-between mx-5">
                                     <span class="font-weight-bold">Event For</span>
-                                    <span>Male</span>
+                                    <span>{{ucfirst($einfo['gallow'])}}</span>
                                 </p>
-                                
+                                <p class="d-flex justify-content-between mx-5">
+                                    <span class="font-weight-bold">Event For class</span>
+                                    <span>{{rtrim(ucfirst($einfo['efor']),"-")}}</span>
+                                </p>
+
                                 <p class="d-flex justify-content-between mx-5">
                                     <span class="font-weight-bold">Event Location</span>
-                                    <span>J.K Ground</span>
+                                    <span>{{ucfirst($einfo['place'])}}</span>
                                 </p>
                             </div>
                         
