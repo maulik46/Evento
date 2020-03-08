bc263a6839d4fe42adbde4c7adb7a27edcde957a
maulik46 <maulikp4646@gmail.com>
2020-03-08 17:15:02 +0530

    design changed

--- resources/views/system/read_request.blade.php
+++ resources/views/system/read_request.blade.php
@@ -1,35 +1,59 @@
 @extends('system/system_layout')
-@section('title','Demo Requests')
+@section('title','Read Requests')
 
 @section('my-content')
 <div class="container-fluid mt-2">
-    <div class="text-center py-2 rounded card shadow-none mb-0">
-        <span class="h4">
-        <i class="uil uil-envelope-edit h4 mr-1"></i>
-            Demo Request
-        </span>
-    </div>
-    <div class="mt-2 rounded-lg p-3 px-1 px-sm-2" style="border:1px solid #e2e7f1;">
-        <div class="mb-3 bg-light new-shadow-sm p-2">
-        <div class="navbar px-0">
-            <div>
-                <span class="ml-2 badge badge-success badge-pill px-3">
-                    12/12/2019
+    <div class="mt-4 rounded-lg p-3 px-1 px-sm-2" style="border:1px solid #e2e7f1;">
+    <div>
+        <div class="d-flex justify-content-center align-items-start align-items-sm-center flex-column flex wrap">
+            <h5>
+            Sutex bank college of computer applications & science
+            </h5>
+            <div class="d-flex flex-sm-row flex-column">
+                <span class="badge badge-pill badge-soft-primary pl-2 pr-3 m-1">
+                <i data-feather="calendar" height="14px"></i>
+                12/12/2019
                 </span>
-                <span class="ml-2 badge badge-primary badge-pill px-3">
-                    By Yash Parmar
+                <span class="badge badge-pill badge-soft-info pl-2 pr-3 m-1">
+                <i data-feather="mail" height="14px"></i>
+                yashparmar@gmail.com
+                </span>
+                <span class="badge badge-pill badge-soft-dark pl-2 pr-3 m-1">
+                <i data-feather="phone" height="14px"></i>
+                9284984923
                 </span>
             </div>
-            <div>
-                <a href="#" class="badge badge-pill pr-sm-2 pl-sm-3 p-1  hover-me-sm text-white d-flex align-items-center" style="background-color: var(--info);">
-                    <span class="d-none d-sm-flex">Read more</span>
-                    <i data-feather="arrow-right-circle" height="16px"></i>
-                </a>
+        </div>
+        <hr>
+        <div class="text-muted">
+        Hello..
+        <br><br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias perferendis, repellendus doloribus ipsum cupiditate atque consectetur nostrum repellat incidunt vero voluptate rem hic perspiciatis impedit explicabo nobis, veritatis consequuntur architecto.
+        <br><br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias perferendis, repellendus doloribus ipsum cupiditate atque consectetur nostrum repellat incidunt vero voluptate rem hic perspiciatis impedit explicabo nobis, veritatis consequuntur architecto.
+        <br><br>
+            <div class="text-muted font-weight-bold">
+                <i data-feather="map-pin" height="18px"></i>Address
+                <div class="m-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit
+                <br>
+                <span>City name</span>
+                </div>
+            </div>
+            <div class="text-right font-weight-bold text-dark m-3">
+            <h6>By Yash Parmar</h6>
             </div>
         </div>
-        <h6 class="my-0 ml-2">Sutex bank college of computer applications & Science</h6>
-        </div>
     </div>
+    </div>
+    <a href="#" class="mt-2 btn btn-success font-weight-bold rounded-sm px-3 new-shadow-sm hover-me-sm">
+        Accept Request
+        <i data-feather="check-square" height="18px"></i>
+    </a>
+    <a href="#" class="mt-2 btn btn-danger font-weight-bold rounded-sm px-3 new-shadow-sm hover-me-sm">
+        Reject
+        <i data-feather="x-circle" height="18px"></i>
+    </a>
 </div>
 @endsection
 
