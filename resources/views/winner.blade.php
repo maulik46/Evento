0e93c1326f5f5835e3a6110eea6851a4e8d2915e
maulik46 <maulikp4646@gmail.com>
2020-03-12 20:36:25 +0530

    table added

--- resources/views/winner.blade.php
+++ resources/views/winner.blade.php
@@ -100,12 +100,64 @@
                 </div>
                 </div>
             </div>
+            <div>
+            
+            </div>
+            <div id="filter-table" class="card-body text-muted mt-3 py-0 px-1">
+                <div class="table-responsive overflow-auto my-scroll">
+                    <table class="table table-hover table-light new-shadow ">
+                        <thead class="light-bg2">
+                            <tr class="text-dark">
+                                <th scope="col">Rank</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Event</th>
+                                <th scope="col">Class</th>
+                                <th scope="col">Division</th>
+                                <th scope="col">Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">
+                                    1
+                                </th>
+                                <td>Piyush Monpara</td>
+                                <td>Cricket</td>
+                                <td>TYBCA</td>
+                                <td>3</td>
+                                <td>12/12/2019</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    2
+                                </th>
+                                <td>Dishant Sakariya</td>
+                                <td>Rakhi making</td>
+                                <td>TYBCA</td>
+                                <td>3</td>
+                                <td>12/12/2019</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    3
+                                </th>
+                                <td>Yash Parmar</td>
+                                <td>Kho-kho</td>
+                                <td>TYBCA</td>
+                                <td>3</td>
+                                <td>12/12/2019</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         <div class="row mt-2">
         @foreach($winners as $winner)
             <?php 
                 $event=App\tblevent::select('eid','ename','e_type','enddate')->where('eid',$winner->eid)->first();
             ?>
-            <div class="card mb-0 my-2 rounded-0 col-lg-6 col-md-12 new-shadow-sm">
+            <div class="col-lg-6 col-md-12 my-2">
+            <div class="card mb-0 rounded-0  new-shadow-sm">
                 <div class="row  align-items-center justify-content-between mx-0" style="background-color:#dde1fc;">
                     <div class="p-2"  >
                         <span class="h5">{{ucfirst($event->ename)}}</span>
@@ -120,7 +172,7 @@
                     </div>
                 </div>
                 <div class="table-responsive">
-                    <table class="table table-hover table-light">
+                    <table class="table table-hover table-light text-dark">
                         <thead class="thead-light">
                             <tr>
                             @if($event->e_type=="solo")
@@ -150,8 +202,8 @@
                                     <img src="assets/images/svg-icons/student-dash/winner/3.svg" height="22px" alt="3">
                                 @endif
                                 </th>
-                                <td>{{$sinfo['sname']}}</td>
-                                <td>{{strtoupper($sinfo['class'])}}</td>
+                                <td class="font-weight-bold">{{ucfirst($sinfo['sname'])}}</td>
+                                <td>{{ucfirst($sinfo['class'])}}</td>
                                 <td>{{$sinfo['division']}}</td>
                             </tr>
                         @else
@@ -165,7 +217,7 @@
                                     <img src="assets/images/svg-icons/student-dash/winner/3.svg" height="22px" alt="3">
                                 @endif
                                 </th>
-                                <td>{{$r['tname']}}</td>
+                                <td class="font-weight-bold">{{$r['tname']}}</td>
                                 <td class="text-center">
                                     <a href="{{url('viewteam')}}/{{encrypt($r['pid'])}}" class="badge badge-success badge-pill px-3">
                                         View
@@ -194,6 +246,7 @@
                     </table>
                 </div>
             </div>
+            </div>
         @endforeach
          
         </div>
@@ -205,192 +258,6 @@
         </div>
     </div>
 
-
-
-
-
-        <!-- <div class="card col-lg-6 col-md-12 p-3 new-shadow-sm">
-            <div class="card mb-0 rounded-0 row flex-row align-items-center justify-content-between mx-0"
-                style="background-color:#dde1fc;">
-                <div class="p-2">
-                    <span class="h5">Cricket Competition</span>
-                    <span class="badge badge-pill badge-info px-3">Solo</span>
-                </div>
-
-                <div class="mr-2">
-                    <span class="badge badge-pill badge-primary px-3">12/02/2020</span>
-                </div>
-            </div>
-            <div class="accordion custom-accordionwitharrow" id="accordionExample">
-                <div class="card mb-1 border">
-                        <div class="card-header px-2" id="heading1">
-                            <div class="d-flex justify-content-between align-items-center">
-                            <div class="d-flex align-items-center">
-                                <img src="assets/images/svg-icons/student-dash/winner/1.svg" height="25px" alt="1">
-                                <span class="ml-2 h5 my-0">Team A</span>
-                            </div>
-                            <a href="#" class="text-dark" data-toggle="collapse" data-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
-                            <span class="badge badge-success badge-pill px-3 new-shadow-sm">Show Team</span>
-                            </a>
-                            </div>
-                        </div>
-                    <div id="collapse1" class="collapse" aria-labelledby="heading1"
-                        data-parent="#accordionExample">
-                        <div class="card-body text-muted py-0 px-1">
-                            <div class="table-responsive">
-                                <table class="table table-hover table-light new-shadow">
-                                    <thead class="thead-light">
-                                        <tr>
-                                            <th scope="col">#</th>
-                                            <th scope="col">Name</th>
-                                            <th scope="col">Class</th>
-                                            <th scope="col">Division</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">
-                                                1
-                                            </th>
-                                            <td>Piyush Monpara</td>
-                                            <td>TYBCA</td>
-                                            <td>3</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">
-                                                2
-                                            </th>
-                                            <td>Dishant Sakariya</td>
-                                            <td>TYBCA</td>
-                                            <td>3</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">
-                                                3
-                                            </th>
-                                            <td>Yash Parmar</td>
-                                            <td>TYBCA</td>
-                                            <td>3</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card mb-1 shadow-none border">
-
-                    <a href="#" class="text-dark" data-toggle="collapse" data-target="#collapse2" aria-expanded="true"
-                        aria-controls="collapse2">
-                        <div class="card-header" id="heading2">
-                            <h5 class="m-0 font-size-16">
-                                Team b
-                                <i data-feather="chevron-down" class="float-right accordion-arrow"></i>
-                            </h5>
-                        </div>
-                    </a>
-                    <div id="collapse2" class="collapse" aria-labelledby="heading2"
-                        data-parent="#accordionExample">
-                        <div class="card-body text-muted">
-                            <div class="table-responsive">
-                                <table class="table table-hover table-light new-shadow">
-                                    <thead class="thead-light">
-                                        <tr>
-                                            <th scope="col">#</th>
-                                            <th scope="col">Name</th>
-                                            <th scope="col">Class</th>
-                                            <th scope="col">Division</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">
-                                                1
-                                            </th>
-                                            <td>Piyush Monpara</td>
-                                            <td>TYBCA</td>
-                                            <td>3</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">
-                                                2
-                                            </th>
-                                            <td>Dishant Sakariya</td>
-                                            <td>TYBCA</td>
-                                            <td>3</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">
-                                                3
-                                            </th>
-                                            <td>Yash Parmar</td>
-                                            <td>TYBCA</td>
-                                            <td>3</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card mb-1 shadow-none border">
-
-                    <a href="#" class="text-dark" data-toggle="collapse" data-target="#collapse3" aria-expanded="true"
-                        aria-controls="collapse3">
-                        <div class="card-header" id="heading3">
-                            <h5 class="m-0 font-size-16">
-                                Team c
-                                <i data-feather="chevron-down" class="float-right accordion-arrow"></i>
-                            </h5>
-                        </div>
-                    </a>
-
-                    <div id="collapse3" class="collapse" aria-labelledby="heading3"
-                        data-parent="#accordionExample">
-                        <div class="card-body text-muted">
-                            <div class="table-responsive">
-                                <table class="table table-hover table-light new-shadow">
-                                    <thead class="thead-light">
-                                        <tr>
-                                            <th scope="col">#</th>
-                                            <th scope="col">Name</th>
-                                            <th scope="col">Class</th>
-                                            <th scope="col">Division</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">
-                                                1
-                                            </th>
-                                            <td>Piyush Monpara</td>
-                                            <td>TYBCA</td>
-                                            <td>3</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">
-                                                2
-                                            </th>
-                                            <td>Dishant Sakariya</td>
-                                            <td>TYBCA</td>
-                                            <td>3</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">
-                                                3
-                                            </th>
-                                            <td>Yash Parmar</td>
-                                            <td>TYBCA</td>
-                                            <td>3</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> -->
 </div>
 @endsection
 
@@ -403,7 +270,7 @@ $(document).ready(function(){
         $('.btn-filter').hide();
     });
     $('#close-btn').click(function(){
-        $('#filter-box,#close-btn').hide();
+        $('#filter-box,#close-btn,#filter-table').hide();
         $('.btn-filter').show();
     })
 })
