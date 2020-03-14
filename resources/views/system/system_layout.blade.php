9dc62ecebdbe59b68effc1421bb411263a09b9bf
parth177 <59960568+parth177@users.noreply.github.com>
2020-03-14 20:33:31 +0530

    Update system_layout.blade.php

--- resources/views/system/system_layout.blade.php
+++ resources/views/system/system_layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <title>@yield('title') | Evento</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!-- App favicon -->
@@ -22,6 +23,22 @@
 <body class="body-scroll pb-0">
 
     <!-- Begin page -->
+    @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+    @if(Session::has('alert-' . $msg))
+           
+           <div class="toast bg-{{$msg}} fade show border-0 new-shadow rounded position-fixed w-75" style="top:80px;right:30px;z-index:9999999;" role="alert" aria-live="assertive" aria-atomic="true" data-toggle="toast">
+               <div class="toast-body text-white alert mb-1">
+                   <a href="#" class=" text-white float-right" data-dismiss="alert" aria-label="Close">
+                       <i data-feather="x-circle" id="close-btn" height="18px" ></i>
+                   </a>
+                   <div class="mt-2 font-weight-bold font-size-14">
+                       {{Session::get('alert-' . $msg)}}
+                   </div> 
+                   
+               </div>
+           </div>
+    @endif
+    @endforeach
     <div id="wrapper">
             <nav class="navbar position-fixed w-100 shadow-none" style="z-index:99!important;">
                 <a href="/index" class="navbar-brand logo-position">
@@ -43,7 +60,7 @@
                     @if(Session::get('syspropic')=="")
                         <img src="{{asset('assets/images/avatars/man.svg')}}" class="avatar-xl border rounded-circle bg-white d-block mx-auto" alt="User" />
                     @else
-                    <img src="{{asset('assets/images/avatars/women.svg')}}" class="avatar-xl border rounded-circle bg-white d-block mx-auto" alt="User" />
+                    <img src="{{asset('profile_pic/admin_pro_pic/')}}/{{Session::get('syspropic')}}" class="avatar-xl border rounded-circle bg-white d-block mx-auto" alt="User" />
                     @endif
                         <div class="media-body mx-auto mt-3 font-weight-bold text-center">
                             <a href="#" class="pro-user-name text-dark user-link font-size-16">
@@ -129,10 +146,11 @@
             
             <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
                 
-                <form class="mt-2">
+                <form class="mt-2" method="post" onsubmit="return valid()" action="{{url('system/update_profile')}}" enctype="multipart/form-data">
                 @csrf
+                <input type="hidden" name="sid" class="form-control" value="{{ucfirst(Session::get('sid'))}}" />
                     <div class="text-center d-flex justify-content-center align-items-start">
-                        <img src="{{asset('assets/images/avatars/child.svg')}}" class="avatar-xl rounded-circle bg-soft-success border" alt="user image">
+                        <img src="{{asset('profile_pic/admin_pro_pic/')}}/{{Session::get('syspropic')}}" class="avatar-xl rounded-circle bg-soft-success border" alt="user image">
                         <label data-toggle="tooltip" data-placement="top" title="Upload Picture" for="photo-upload" class="custom-file-upload w-auto overflow-auto border-0 position-relative btn-profile p-1 btn-rounded" style="left:-15px;" >
                         <i id="camera" height="16px" class="icon-dual-primary" data-feather="edit" ></i>
                         <!-- <span id="up" class="mx-2">Upload Picture</span> -->
@@ -140,25 +158,24 @@
                         <input id="photo-upload" name="photo-upload" type="file" />
                     </div>
                     
-                    
                     <label class="col-form-label font-size-14">Name</label>
                     <div class="form-group has-icon d-flex align-items-center">
                         <i class="uil uil-user form-control-icon ml-2 font-size-16"></i>
                         <input type="text" id="sname" value="{{ucfirst(Session::get('sysadmin'))}}" name="user_name" class="form-control" value="" />
                     </div>
-                    <div id="name-er" class="text-danger font-weight-bold"></div>
+                    <div id="name-err" class="text-danger font-weight-bold"></div>
                     <label class="col-form-label font-size-14">Email</label>
                     <div class="form-group has-icon d-flex align-items-center">
                         <i class="uil uil-envelope form-control-icon ml-2 font-size-16"></i>
-                        <input type="email" id="semail" value="{{Session::get('sysemail')}}" name="user_email" class="form-control" value="" />
+                        <input type="text" id="semail" value="{{Session::get('sysemail')}}" name="user_email" class="form-control" value="" />
                     </div>
-                    <div id="email-er" class="text-danger font-weight-bold"></div>
+                    <div id="email-err" class="text-danger font-weight-bold"></div>
                     <label class="col-form-label font-size-14">Mobile No</label>
                         <div class="form-group has-icon d-flex align-items-center">
                             <i class="uil uil-phone-alt form-control-icon ml-2 font-size-16"></i>
                         <input type="text" id="smobile" value="{{Session::get('sysmobile')}}" name="user_mobile" class="form-control" value="" />
                     </div>
-                    <div id="mobile-er" class="text-danger font-weight-bold"></div>
+                    <div id="mobile-err" class="text-danger font-weight-bold"></div>
                     
 
                     <button type="submit" class="mt-2 btn  rounded-sm hover-me-sm px-3 font-weight-bold new-shadow-sm btn-sm py-2 font-size-13 text-white" style="background-color: var(--success);">
@@ -169,26 +186,28 @@
             </div>
             <div class="tab-pane fade  active" id="pills-password" role="tabpanel" aria-labelledby="pills-password-tab">
                 
-                <form>
+                <form method="post" action="{{url('system/update_pass')}}" onsubmit="return check()">
+                @csrf
+                <p id="error" class="text-center text-danger font-weight-bold"></p>
                     <div class="form-group mt-2">
                         <label class="col-form-label font-size-14">Current Password</label>
                         <div class="form-group has-icon d-flex align-items-center">
                             <i class="uil uil-padlock font-size-17 form-control-icon ml-2"></i>
-                            <input type="password"  class="form-control" placeholder="Enter Current Password..." />
+                            <input type="password" id="curpass" name="current_pass"  class="form-control" placeholder="Enter Current Password..." />
                         </div>
                     </div>
                     <div class="form-group mt-2">
                         <label class="col-form-label font-size-14">New Password</label>
                         <div class="form-group has-icon d-flex align-items-center">
                             <i class="uil uil-lock-open-alt font-size-17 form-control-icon ml-2"></i>
-                            <input type="password" class="form-control" placeholder="Enter New Password..." />
+                            <input type="password" id="npass" name="npass" class="form-control" placeholder="Enter New Password..." />
                         </div>
                     </div>
                     <div class="form-group mt-2">
                         <label class="col-form-label font-size-14">Confirm Password</label>
                         <div class="form-group has-icon d-flex align-items-center">
                             <i class="uil uil-padlock font-size-17 form-control-icon ml-2"></i>
-                            <input type="password"  class="form-control" placeholder="Enter Confirm Password..." />
+                            <input type="password" id="cpass" name="cpass" class="form-control" placeholder="Enter Confirm Password..." />
                         </div>
                     </div>
                     <button type="submit" class="hover-me-sm btn btn-success rounded-sm new-shadow-sm font-weight-bold px-3 mt-2 mb-1">
@@ -212,7 +231,68 @@
     <script src="{{asset('assets/libs/moment/moment.min.js')}}"></script>
     <script src="{{asset('assets/js/app.min.js')}}"></script>
     @section('extra-scripts')
+<script>
+function valid()
+{
+    var f=0;
+    var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if($('#sname').val()=="")
+    {
+        $('#name-err').text("Please enter name");
+        f=1;
+    }
+    else{
+        $('#name-err').text("");
+    }
+   if(!regex.test($('#semail').val()))
+    {
+        
+        $('#email-err').text("Invalid email Address");
+        f=1;
+    }
+    else{
+        $('#email-err').text("");
+    }
 
+    regex = /^\d*(?:\.\d{1,2})?$/; 
+    var mo= $('#smobile').val();
+    if(!(regex.test($('#smobile').val()) && mo.length == 10))
+    {
+        $('#mobile-err').text("Please valid Mobile no.");
+        f=1;
+    }
+    else{
+        $('#mobile-err').text("");
+    }
+    if(f==1)
+    {
+        return false;
+    }
+}
+function check()
+{
+    if($('#curpass').val()=="")
+    {
+        $('#error').html("Plese Enter your current password");
+        return false;
+    }
+    if($('#npass').val()=="")
+    {
+        $('#error').html("Plese Enter your New password");
+        return false;
+    }
+    else if($('#npass').val().length <= 6)
+    {
+        $('#error').html("New password length must be greater then 6 character");
+        return false;
+    }
+    if($('#cpass').val()=="")
+    {
+        $('#error').html("Please Re-Enter your New password");
+        return false;
+    }
+}
+</script>
     @show
 </body>
 </html>
