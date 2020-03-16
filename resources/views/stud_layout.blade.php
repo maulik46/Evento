4b573ee5646d102b914357fe82097414e3d9238c
parth177 <59960568+parth177@users.noreply.github.com>
2020-03-16 18:07:53 +0530

    Update stud_layout.blade.php

--- resources/views/stud_layout.blade.php
+++ resources/views/stud_layout.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <title>@yield('title')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!-- App favicon -->
