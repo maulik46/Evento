10e8db81c62a3e7a7ba01dc3a0b20948df5634f7
parth177 <59960568+parth177@users.noreply.github.com>
2020-03-16 17:42:01 +0530

    Update winner.blade.php

--- resources/views/winner.blade.php
+++ resources/views/winner.blade.php
@@ -37,114 +37,77 @@
                 <div class="col-md-5 col-12">
                     <div class="form-group has-icon d-flex align-items-center px-0 mb-1">
                         <i data-feather="user" class="form-control-icon ml-2" height="19px"></i>
-                        <input type="text" name="title" id="title" class="form-control" placeholder="Enter Student Name" />
+                        <input type="text" name="sname" onkeyup="filter()" id="sname" class="form-control" placeholder="Enter Student Name" />
                     </div>
                 </div>
                 <div class="col-md-5 col-12">
                     <div class="form-group has-icon d-flex align-items-center px-0 mb-1">
                         <i data-feather="calendar" class="form-control-icon ml-2" height="19px"></i>
-                        <input type="text" name="title" id="title" class="form-control" placeholder="Enter Event Name" />
+                        <input type="text" name="ename" onkeyup="filter()" id="ename" class="form-control" placeholder="Enter Event Name" />
                     </div>
                 </div>
                 <div class="col-md-2 col-12">
-                        <select name="" id="" class="form-control">
-                            <option hidden>Select Year</option>
-                            <option value="">2018</option>
-                            <option value="">2019</option>
-                            <option value="">2020</option>
+                        <select name="year" id="year" onchange="filter()" class="form-control">
+                        <?php $year=date('Y')?>
+                            <option hidden value="">Select Year</option>
+                            @for($i=2019;$i<=$year;$i++)
+                            <option value="{{$i}}">{{$i}}</option>
+                            @endfor
                         </select>
                 </div>
                 </div>
                 <div class="row justify-content-between justify-content-sm-around">
                     <div class="col-auto">
                         <h6>Class</h6>
+                        <?php $c=0?>
+                        @foreach($ddclass as $class)
+                        <?php $c++?>
                         <div class="custom-control custom-checkbox mb-2">
-                            <input type="checkbox" class="custom-control-input" id="customCheck1">
-                            <label class="custom-control-label" for="customCheck1">Fybca</label>
+                            <input type="checkbox" name="clas[]" onclick="filter()" value="{{$class->class}}" class="custom-control-input" id="clas{{$c}}">
+                            <label class="custom-control-label" for="clas{{$c}}">{{ucfirst($class->class)}}</label>
                         </div>
-                        <div class="custom-control custom-checkbox mb-2">
-                            <input type="checkbox" class="custom-control-input" id="customCheck1">
-                            <label class="custom-control-label" for="customCheck1">Sybca</label>
-                        </div>
-                        <div class="custom-control custom-checkbox mb-2">
-                            <input type="checkbox" class="custom-control-input" id="customCheck1">
-                            <label class="custom-control-label" for="customCheck1">Tybca</label>
-                        </div>
+                        @endforeach
                     </div>
                     <div class="col-auto">
                         <h6>Division</h6>
                         <div class="custom-control custom-checkbox mb-2">
-                            <input type="checkbox" class="custom-control-input" id="customCheck1">
-                            <label class="custom-control-label" for="customCheck1">1</label>
+                            <input type="checkbox" name="div[]" onclick="filter()" value="1" class="custom-control-input" id="div1">
+                            <label class="custom-control-label" for="div1">1</label>
                         </div>
                         <div class="custom-control custom-checkbox mb-2">
-                            <input type="checkbox" class="custom-control-input" id="customCheck1">
-                            <label class="custom-control-label" for="customCheck1">2</label>
+                            <input type="checkbox" name="div[]" onclick="filter()" value="2" class="custom-control-input" id="div2">
+                            <label class="custom-control-label" for="div2">2</label>
                         </div>
                         <div class="custom-control custom-checkbox mb-2">
-                            <input type="checkbox" class="custom-control-input" id="customCheck1">
-                            <label class="custom-control-label" for="customCheck1">3</label>
+                            <input type="checkbox" name="div[]" onclick="filter()" value="3" class="custom-control-input" id="div3">
+                            <label class="custom-control-label" for="div3">3</label>
+                        </div>
+                        <div class="custom-control custom-checkbox mb-2">
+                            <input type="checkbox" name="div[]" onclick="filter()" value="4" class="custom-control-input" id="div4">
+                            <label class="custom-control-label" for="div4">4</label>
                         </div>
                     </div>
                     <div class="col-auto">
-                        <h6>Category</h6>
+                        <h6>Event type</h6>
                         <div class="custom-control custom-checkbox mb-2">
-                            <input type="checkbox" class="custom-control-input" id="customCheck1">
-                            <label class="custom-control-label" for="customCheck1">Team</label>
+                            <input type="checkbox" name="cat[]" onclick="filter()" value="team" class="custom-control-input" id="cat1">
+                            <label class="custom-control-label" for="cat1">Team</label>
                         </div>
                         <div class="custom-control custom-checkbox mb-2">
-                            <input type="checkbox" class="custom-control-input" id="customCheck1">
-                            <label class="custom-control-label" for="customCheck1">Solo</label>
+                            <input type="checkbox" name="cat[]" onclick="filter()"p value="solo" class="custom-control-input" id="cat2">
+                            <label class="custom-control-label" for="cat2">Solo</label>
                         </div>
                     </div>
                 </div>
                 </div>
             </div>
+            <div>
+            
+            </div>
             <div id="filter-table" class="card-body text-muted mt-3 py-0 px-1">
                 <div class="table-responsive overflow-auto my-scroll">
-                    <table class="table table-hover table-light new-shadow ">
-                        <thead class="light-bg2">
-                            <tr class="text-dark">
-                                <th scope="col">Rank</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Event</th>
-                                <th scope="col">Class</th>
-                                <th scope="col">Division</th>
-                                <th scope="col">Date</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">
-                                    1
-                                </th>
-                                <td>Piyush Monpara</td>
-                                <td>Cricket</td>
-                                <td>TYBCA</td>
-                                <td>3</td>
-                                <td>12/12/2019</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    2
-                                </th>
-                                <td>Dishant Sakariya</td>
-                                <td>Rakhi making</td>
-                                <td>TYBCA</td>
-                                <td>3</td>
-                                <td>12/12/2019</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    3
-                                </th>
-                                <td>Yash Parmar</td>
-                                <td>Kho-kho</td>
-                                <td>TYBCA</td>
-                                <td>3</td>
-                                <td>12/12/2019</td>
-                            </tr>
-                        </tbody>
+                    <table class="table table-hover table-light new-shadow" id="tbody">
+                       
                     </table>
                 </div>
             </div>
@@ -247,6 +210,10 @@
         @endforeach
          
         </div>
+            
+        <div class="row mt-4">
+            
+        </div>
         <!-- end row tag -->
         </div>
     </div>
@@ -267,6 +234,55 @@ $(document).ready(function(){
         $('.btn-filter').show();
     })
 })
+</script>
+<script>
+function filter()
+{
+    var ename=$('#ename').val();
+    var sname=$('#sname').val();
+    var clas =new Array();
+    var div = new Array();
+    var cat = new Array();
+    var year=$('#year').val();
+    $("input[name='clas[]']").each( function () {
+        if($(this).prop('checked') == true){
+            clas.push($(this).val());
+            }
+   });
+   $("input[name='div[]']").each( function () {
+        if($(this).prop('checked') == true){
+            div.push($(this).val());
+            }
+   });
+   $("input[name='cat[]']").each( function () {
+        if($(this).prop('checked') == true){
+            cat.push($(this).val());
+            }
+   });
+   $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        $.ajax({
+            type: 'POST',
+            url: '/filter',
+            data: {
+               clas:clas,
+               div:div,
+               cat:cat,
+               year:year,
+               sname:sname,
+               ename:ename,
+            },
+            success: function (data) {
+                $('#tbody').html(data.msg);
+            },
+            error: function (data) {
+                console.log(data);
+            }
+        })
+}
 </script>
 @endsection
 
