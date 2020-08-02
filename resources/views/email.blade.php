abcabcfc597fa4d24861aed3ed7aff7a252e79f9
yash-201 <59959814+yash-201@users.noreply.github.com>
2020-08-02 18:07:46 +0530

    Update email.blade.php

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,6 +1,2 @@
-<h3>Evento Login Authentication</h3>
-<div style="font-size:13px;">
-<strong><u>{{ $name  }}</u></strong> is your OTP for Login in Evento. <u>Please do not share this OTP to anyone.</u>
-</div>
-<div style="margin-top:5px;">From,</div>
+
 {!! $body !!}
